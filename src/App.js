e473bbfd123d762408ad75d3a4392997d97b67b0
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-10 19:28:35 +0430

    add highlight and discount section

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './resources/styles.css'
 import Header from './components/Header-Footer/Header'
 import Featured from './components/Featured'
 import VenueInfo from './components/VenueInfo'
-
+import Highlights from './components/Highlights'
 
 class App extends Component {
   render() {
     return (
       <div className="App"
-        style={{ height: "1500px", background: "lime" }}
+        style={{
+          height: "1500px",
+          // background: "lime"
+        }}
       >
         <Header />
         <Featured />
-        <VenueInfo/>
+        <VenueInfo />
+        <Highlights/>
       </div>
     );
   }
---
+++ src/components/Highlights/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Description from './Decription'
+import Discount from './Discount'
+
+
+
+export default function Highlights() {
+    return (
+        <div className="highlight_wrapper">
+            <Description />
+            <Discount/>
+        </div>
+    )
+}
+ 
