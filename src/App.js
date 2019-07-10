d155f16f93787e5af43ecde54ce662313562ca61
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-10 13:17:24 +0430

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './resources/styles.css'
-import Header from'./components/Header-Footer/Header'
-
+import Header from './components/Header-Footer/Header'
+import Featured from './components/Featured'
 
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height:"1500px",background:"lime"}} >
+      <div className="App"
+        style={{ height: "1500px", background: "lime" }}
+      >
         <Header />
-     
+        <Featured />
       </div>
     );
   }
---
+++ src/components/Featured/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Carousel from './Carousel'
+export default function Featured() {
+    return (
+        <div style={{position:"relative"}}>
+            <Carousel/>
+            
+            <div className="artist_name">
+                <div className="wrapper">
+                    Ariana Grande 
+                </div>
+            </div>
+        </div>
+    )
+}
