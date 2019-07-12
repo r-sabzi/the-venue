d23a3872e9799aa7832104731bce63b7ea4745ba
r.sabzi <r.kh.sabzi@gmail.com>
2019-07-12 18:47:21 +0430

    minor clean up

--- src/components/Header-Footer/SideDrawer.js
+++ src/components/Header-Footer/SideDrawer.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Drawer from '@material-ui/core/Drawer'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import {scroller} from 'react-scroll'
-import Highlights from '../Highlights';
+import { scroller } from 'react-scroll'
 
 
 
@@ -15,11 +14,11 @@ export default function SideDrawer(props) {
             duration: 1500,
             delay: 100,
             smooth: true,
-            offset:-85
+            offset: -85
         })
         props.onClose(false)
     }
-    
+
 
 
 
@@ -33,23 +32,23 @@ export default function SideDrawer(props) {
             >
 
                 <List component="nav">
-                    <ListItem button onClick={() => { scrollToElement('featured')}}>
+                    <ListItem button onClick={() => { scrollToElement('featured') }}>
                         Events starts in
                     </ListItem>
-                    <ListItem button onClick={() => {scrollToElement('venueInfo') }}>
+                    <ListItem button onClick={() => { scrollToElement('venueInfo') }}>
                         Venue info
                     </ListItem>
-                    <ListItem button onClick={() => { scrollToElement('highlights')}}>
+                    <ListItem button onClick={() => { scrollToElement('highlights') }}>
                         Highlights
                     </ListItem>
-                    <ListItem button onClick={() => { scrollToElement('pricing')}}>
+                    <ListItem button onClick={() => { scrollToElement('pricing') }}>
                         Pricing
                     </ListItem>
-                    <ListItem button onClick={() => { scrollToElement('location')}}>
+                    <ListItem button onClick={() => { scrollToElement('location') }}>
                         Location
                     </ListItem>
                 </List>
-                
+
             </Drawer>
         </div>
     )
--- src/components/Highlights/Decription.js
+++ src/components/Highlights/Decription.js
@@ -13,6 +13,6 @@ export default function Decription() {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis est dolorem eos quam ullam neque excepturi adipisci, architecto, dolores quidem doloremque tempora a deserunt dolorum nulla maiores. Ut, ex.
                 </div>
             </div>
-      </Fade>
+        </Fade>
     )
 }
